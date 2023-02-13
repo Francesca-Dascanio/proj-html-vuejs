4f4f04d7e51b98b7bddd078d8624fb2134da9d1a
“Francesca <dascanio.francesca@gmail.com>
2023-02-13 16:39:08 +0100

    Struttura layout > main: section-4, section-5

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -127,11 +127,65 @@ export default {
         </section>
 
         <section id="section-4">
-            
+            <div class="container-lg">
+                <div class="container-sm">
+                    <div class="section-title text-center py-md">
+                        <h5>
+                        Lorem ipsum dolor sit.
+                        </h5>
+                        <h2>
+                            SECTION 4 TITLE
+                        </h2>
+                    </div>
+
+                    <div class="section-contents flex align-items py-md">
+                        <div class="cards flex">
+                            <div class="card text-center py-md">
+                                Card 1
+                            </div>
+                            <div class="card text-center py-md">
+                                Card 2
+                            </div>
+                            <div class="card text-center py-md">
+                                Card 3
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <section id="section-5">
-            
+            <div class="container-lg">
+                <div class="container-sm">
+                    <div class="section-title text-center py-md">
+                        <h5>
+                        Lorem ipsum dolor sit.
+                        </h5>
+                        <h2>
+                            SECTION 5 TITLE
+                        </h2>
+                    </div>
+
+                    <div class="section-contents flex align-items py-md">
+                        <div class="cards flex">
+                            <div class="card text-center py-md">
+                                Card 1
+                            </div>
+                            <div class="card text-center py-md">
+                                Card 2
+                            </div>
+                            <div class="card text-center py-md">
+                                Card 3
+                            </div>
+                        </div>
+
+                        <button>
+                            Button
+                        </button>
+                    </div>
+                </div>
+            </div>
         </section>
         
     </main>
@@ -152,7 +206,6 @@ export default {
         }
     }
 }
-
 
 #section-2 {
     .container-lg {
@@ -218,6 +271,48 @@ export default {
 
                 }
 
+        }
+    }
+}
+
+#section-4 {
+    .container-lg {
+    background-color: lightblue;
+    // Verifica in relazione all'altezza della sub-section-b
+    margin-top: 165px;
+        .container-sm {
+            background-color: lightcoral;
+
+            .section-contents {
+                .cards {
+
+                    width: 100%;
+                    .card {
+                        width: calc(100% / 3);
+                    }
+                }
+            }
+        }
+    }
+}
+
+#section-5 {
+    .container-lg {
+    background-color: lightblue;
+        .container-sm {
+            background-color: pink;
+
+            .section-contents {
+                flex-direction: column;
+
+                .cards {
+
+                    width: 100%;
+                    .card {
+                        width: calc(100% / 3);
+                    }
+                }
+            }
         }
     }
 }
